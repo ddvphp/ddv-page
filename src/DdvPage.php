1350b57a83e6615ecf1b550f828f6cb0349e2327
hua <yuchonghua@163.com>
2017-05-19 16:54:31 +0800

    fixbug

--- src/DdvPage.php
+++ src/DdvPage.php
@@ -5,13 +5,15 @@ use const null;
 
 
 class DdvPage {
-  public $listsArray = null;
-  public $pageArray = array();
+  protected $flagPageLists = true;
+  protected $listsArray = null;
+  protected $pageArray = array();
   public function __construct($obj)
   {
-    if(is_array($obj)){
-      $this->init($obj);
-    }elseif(is_object($obj)){
+    // 初始化
+    is_array($obj) ? call_user_func_array(array($this, 'init'), func_get_args()) : $this->init(array());
+    // 如果是一个对象
+    if(is_object($obj)){
       call_user_func_array(array($this, 'initByObj'), func_get_args());
     }
   }
@@ -19,78 +21,57 @@ class DdvPage {
   {
     $className = get_class($obj);
     switch ($className) {
+      // 如果是 分页对象
       case 'Illuminate\Pagination\LengthAwarePaginator':
         if (class_exists('Illuminate\Pagination\LengthAwarePaginator')) {
           call_user_func_array(array($this, 'LengthAwarePaginatorInit'), func_get_args());
         }
         break;
+      // 如果是 数据库模型对象
       case 'Illuminate\Database\Eloquent\Builder':
         if (class_exists('Illuminate\Database\Eloquent\Builder')) {
           call_user_func_array(array($this, 'DatabaseBuilderInit'), func_get_args());
         }
         break;
-      
+
       default:
         break;
     }
   }
-  public function DatabaseBuilderInit($obj, $pageNow = 1, $pageSize = 10, $columns = ['*'])
+  public function DatabaseBuilderInit($obj, $pageSize = null, $columns = ['*'], $pageNow = null)
   {
-    $this->LengthAwarePaginatorInit($obj->paginate($pageSize, $columns, 'pageNow', $pageNow));
+    if (empty($pageSize)) {
+      $this->pageArray = null;
+      $this->listsArray = $obj->get($columns);
+    }else{
+      $pageNow = empty($pageNow) ? $this->pageArray['inputPage'] : intval($pageNow);
+      $pageSize = empty($pageSize) || $pageSize === true ? $this->pageArray['size'] : intval($pageSize);
+      $this->LengthAwarePaginatorInit($obj->paginate($pageSize, $columns, 'pageNow', $pageNow));
+    }
   }
   public function LengthAwarePaginatorInit($obj)
   {
-    $this->pageArray = array_merge($this->pageArray, array(
-      //当前页数
-      'now'=>$obj->currentPage(),
-      //输入的页数
-      'input_page'=>$obj->currentPage(),
-      //数据库数据总条数
-      'count'=>$obj->total(),
-      //每页显示条数
-      'size'=>$obj->perPage(),
-      //最后一页是第几页
-      'end'=>$obj->lastPage(),
-      //上一页页数是
-      'before'=>$obj->currentPage(),
-      //下一页页数是
-      'after'=>$obj->currentPage() + 1,
-      //页数列表
-      'lists'=>array(),
-      //是否为传入的页数
-      'is_input_page'=>true,
-      //是否到达尾页
-      'is_end'=>false
-    ));
-    if ($this->pageArray['before']<1) {
-      $this->pageArray['before'] = 1;
-    }
-    if ($this->pageArray['after']>$this->pageArray['end']) {
-      $this->pageArray['after'] = $this->pageArray['end'];
-    }
-    $this->pageArray['is_end'] = $this->pageArray['after'] === $this->pageArray['end'];
-    $this->init($this->pageArray);
-    $this->__setup();
+    $this->pageArray['now'] = intval($obj->currentPage());
+    $this->pageArray['count'] = intval($obj->total());
+    $this->pageArray['size'] = intval($obj->perPage());
+    $this->pageArray['end'] = intval($obj->lastPage());
+    $this->setup();
     $lists = array();
-    $items = $obj->items();
-    $itemsLen = count($items);
-    for($i=0; $i<$itemsLen;$i++){
-      $lists[$i] = $items[$i]->toArray();
+    foreach ($obj->items() as $index => $item) {
+      $lists[$index] = $item->toArray();
     }
-    $this->listsArray = empty($lists)?$this->pageArray:$lists;
+    $this->listsArray = empty($lists) ? $this->listsArray : $lists;
     return;
   }
-  public function init($params = array(),$flag=true)
+  public function init($params = array(),$flag=null)
   {
-    (isset($this->pageArray)&&is_array($this->pageArray)) || ($this->pageArray = array());
-    (isset($c)&&is_array($c)) || ($c = array());
-    $this->C(array(
+    $this->pageArray = array_merge($this->pageArray, array(
       //当前页数
-      'now'=>1,
+      'now'=>null,
       //数据库数据总条数
       'count'=>0,
       //每页显示条数
-      'size'=>10,
+      'size'=>null,
       //最后一页是第几页
       'end'=>10,
       //上一页页数是
@@ -102,24 +83,56 @@ class DdvPage {
       //页数列表
       'lists'=>array(),
       //输入的页数
-      'input_page'=>1,
+      'inputPage'=>null,
       //是否到达尾页
-      'is_end'=>false,
+      'isEnd'=>false,
       //是否为传入的页数
-      'is_input_page'=>true,
+      'isInputPage'=>true,
       //limit开始位置
       'limitStart'=>0
-    ));
-    $this->C($params);
-    $this->__setup($flag);
-  
+    ), $params);
+    $pageNow = null;
+    $pageSize = null;
+    $dataInput = array($_GET, $_POST, $_REQUEST);
+    foreach ($dataInput as $index => $data) {
+      if (empty($pageNow)) {
+        $pageNow = empty($data['page_now']) ? $pageNow : $data['page_now'];
+        $pageNow = empty($data['pageNow']) ? $pageNow : $data['pageNow'];
+      }
+      if (empty($pageSize)) {
+        $pageSize = empty($data['page_size']) ? $pageSize : $data['page_size'];
+        $pageSize = empty($data['pageSize']) ? $pageSize : $data['pageSize'];
+      }
+    }
+    if (empty($this->pageArray['now']) && empty($this->pageArray['inputPage'])) {
+      $this->pageArray['inputPage'] = $this->pageArray['now'] = empty($pageNow) ? 1 : $pageNow ;
+    }
+    if (empty($this->pageArray['size'])) {
+      $this->pageArray['size'] = empty($pageSize) ? 10 : $pageSize ;
+    }
+    $this->setup($flag);
     return $this;
   }
-  private function __setup($flag=true){
-
-    $c = $this->C();
+  public function getLimit(){
+    $r = array($this->pageArray['limitStart'], $this->pageArray['size']);
+    return $r ;
+  }
+  public function getPage($columns = array()){
+    if (empty($columns)) {
+      return $this->pageArray;
+    }else{
+      $r = array();
+      foreach ($columns as $index => $key) {
+        $r[$key] = $this->pageArray[$key];
+      }
+      return $r;
+    }
+  }
+  public function setup($flag=null){
+    $this->flagPageLists = is_null($flag) ? $this->flagPageLists : $flag;
+    $c = &$this->pageArray;
     $c['now'] = intval($c['now']);
-    $c['input_page'] = $c['now'];
+    $c['inputPage'] = $c['now'];
     $c['count'] = intval($c['count']);
     $c['size'] = intval($c['size']);
     if ($c['now']<1) {
@@ -170,88 +183,28 @@ class DdvPage {
         };
       };
     };
-    if(!$flag){
-      $c['lists']=array();
+    if(!$this->flagPageLists){
+      unset($c['lists']);
     }else{
       $c['lists'] = array_combine(array_merge(array_unique($c['lists'])),array_merge(array_unique($c['lists'])));
     }
-    
-
 
 
     //判断是否到了尾页
-    $c['is_end'] = (bool)($c['now'] == $c['end']) ;
+    $c['isEnd'] = (bool)($c['now'] == $c['end']) ;
     //判断是否为传入输入的页数
-    $c['is_input_page'] = (bool)($c['input_page'] == $c['now']) ;
-    $this->C($c);
-  }
-  public function getLimit(){
-    $c = &$this->C();
-    $r = array($c['limitStart'],$c['size']);
-    return $r ;
-  }
-  public function getPage(){
-    return $this->C();  
-  }
+    $c['isInputPage'] = (bool)($c['inputPage'] == $c['now']) ;
 
-
-  /*
-  C('name','value');储存
-  C('name.name1.name2','value');储存
-  C('name.name1.name2');读取
-  C(array('a'=>2));存储
-  */
-  private function &C(){
-    $r = NULL ;
-    $c = &$this->pageArray ;
-    $c_tmp_1 = '';
-    $c_tmp_2 = '';
-    $i = 0 ;
-    $c_names = array();
-    $c_names_len = 0 ;
-    $num = func_num_args() ;
-    $args = func_get_args() ;
-    if($num==0){
-      $r = $c ;
-    }elseif ($num==1&&isset($args[0])&&is_array($args[0])) {
-      $c = array_merge($c,$args[0]) ;
-      $r = TRUE ;
-    }elseif (($num==1||$num==2)&&isset($args[0])&&is_string($args[0])) {
-      $c_names = explode('.',$args[0]);
-      $c_names_len = count($c_names) ;
-      $c_tmp_1 = &$c ;
-      for ($i=0; $i < $c_names_len ; $i++) {
-        if(!empty($c_names[$i])){
-          $c_tmp_2='';
-          unset( $c_tmp_2 ) ;
-          if (isset($c_tmp_1[$c_names[$i]])) {
-            $c_tmp_2 = &$c_tmp_1[$c_names[$i]] ;
-            unset($c_tmp_1) ;
-            $c_tmp_1 = &$c_tmp_2 ;
-            $r =  $c_tmp_1;
-          }elseif ($num==2) {
-            $c_tmp_1[$c_names[$i]] = array() ;
-            $c_tmp_2 = &$c_tmp_1[$c_names[$i]] ;
-            unset($c_tmp_1) ;
-            $c_tmp_1 = &$c_tmp_2 ;
-          }else{
-            $r = NULL ;
-          }
-          unset( $c_tmp_2 );
-          $c_tmp_2='';
-        }
-      }
-      if ($num==2) {
-        $c_tmp_1 = $args[1] ;
-      }
-    }
-    unset($c) ;unset($num) ;unset($args) ;
-    return $r ;
+    $c['limit_start'] = &$c['limitStart'];
+    $c['lists_size'] = &$c['listsSize'];
+    $c['input_page'] = &$c['inputPage'];
+    $c['is_end'] = &$c['isEnd'];
+    $c['is_input_page'] = &$c['isInputPage'];
   }
   public function toArray(){
     return array(
-      'lists'=>$this->listsArray,
-      'page'=>$this->pageArray
+        'lists'=>$this->listsArray,
+        'page'=>$this->pageArray
     );
   }
   /**
